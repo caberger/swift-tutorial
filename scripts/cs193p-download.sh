7db5da41cacc2cae4b54573c9a6560aa6d104ef6
Christian Aberger <c.aberger@htl-perg.ac.at>
2022-08-29 14:42:18 +0200

    download missing files from alternate source

--- scripts/cs193p-download.sh
+++ scripts/cs193p-download.sh
@@ -2,13 +2,25 @@
 # Download the material for NVS_MC_Swift Winter 2022/23
 # (c) Dipl.-Ing. Christian Aberger (2022)
 
-OUTPUT_FOLDER=./target/
+OUTPUT_FOLDER=./target
 BASE_URL=https://cs193p.sites.stanford.edu/sites/g/files/sbiybj16636/files/media/file
+ALT_URL=https://github.com/weitieda/cs193p-2020-swiftui/raw/master/documents/Slides
 
 mkdir -p $OUTPUT_FOLDER
 
-for lecture in {1..14}
-do
-    curl -o ${OUTPUT_FOLDER}/l${lecture}.pdf $BASE_URL/l${lecture}.pdf
-done
+downloadOrig() {
+    curl --location -o ${OUTPUT_FOLDER}/l${2}.pdf $1/l${2}.pdf
+}
 
+for lecture in {1..6}
+do
+    downloadOrig $BASE_URL ${lecture}
+done
+for lecture in {7..8}
+do
+    downloadOrig $ALT_URL ${lecture}
+done
+for lecture in {9..14}
+do
+    downloadOrig $BASE_URL ${lecture}
+done
