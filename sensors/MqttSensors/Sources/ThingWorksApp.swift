f4e66e9c36132fd182f7dbaded7d05545e165f99
caberger <c.aberger@htl-leonding.ac.at>
2023-10-26 22:01:56 +0200

    refactor startup

--- sensors/MqttSensors/Sources/ThingWorksApp.swift
+++ sensors/MqttSensors/Sources/ThingWorksApp.swift
@@ -4,13 +4,9 @@ fileprivate let locationManager = LocationViewModel()
 
 @main
 struct ThingWorksApp: App {
-    var contentView: LocationView
-    init() {
-        contentView = LocationView()
-    }
     var body: some Scene {
         WindowGroup {
-            contentView
+            LocationView()
         }
     }
 }
